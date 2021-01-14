90d07b381d5c4a601ac8f31a355e27f0524f3e53
Elwick1 <michaelrwilliamson92@gmail.com>
2021-01-14 13:14:51 +0000

    adding to github, have scaffolded rails api managed to check database connection is working, used console to add new post to db after seeding original 3 posts

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.create([
+    {
+        title: "Post One",
+        body: "This is the first post, a test if you will"
+    },
+    {
+        title: "Post Two - Electric Boogaloo",
+        body: "The second post, a further test"
+    },
+    {
+        title: "The Final Post",
+        body: "The final stand of post testing"
+    }
+])
